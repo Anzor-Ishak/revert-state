6125511a7fa20bf3410afa4615242083b24a10f3
Anzor-Ishak <anzori@spacelift.io>
2025-09-05 15:09:27 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    random = { source = "hashicorp/random", version = "~> 3.6" }
+    random = { source = "hashicorp/random", version = "~> 3.7" }
   }
 }
 
